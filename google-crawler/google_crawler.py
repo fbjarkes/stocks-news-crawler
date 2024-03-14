209aed5e083fac26183c5fe62ff24ae6d95fd02a
fbjarkes <fbjarkes-github.q5706@aleeas.com>
2024-03-14 12:40:57 +0100

    Add google crawler

---
+++ google-crawler/google_crawler.py
@@ -0,0 +1,41 @@
+import sys
+sys.path.insert(0, '/Users/fbjarkes/git/python/stock-catalyst-news-crawler') # so we can run this file
+
+import json
+from bs4 import BeautifulSoup
+import requests
+import re
+import mechanicalsoup
+
+from link_parsers import newspaper_parser
+
+
+
+def google_search(query):
+    browser = mechanicalsoup.StatefulBrowser()
+    urls = []
+    
+    browser.session.headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9'
+    }
+    
+    browser.open("https://www.google.com/")
+    browser.select_form('form[action="/search"]')
+    browser["q"] = query
+    browser.submit_selected()
+
+    search_results = browser.get_current_page().select('.tF2Cxc')
+    print(f"Found {len(search_results)} results for '{query}'")
+    for idx, result in enumerate(search_results, start=1):
+        title = result.select_one('.DKV0Md').text
+        url = result.select_one('a')['href']
+        print(f"Result {idx}: {title} - {url}")
+        urls.append(url)
+    
+    return urls
+
+if __name__ == "__main__":
+    query = "BAND Bandwidth Inc 2022-11-02"
+    urls = google_search(query)
+    newspaper_parser.parse('google', 'BAND', urls, valid_date='2022-11-02')
---
+++ link_parsers/newspaper_parser.py
@@ -0,0 +1,53 @@
+
+
+
+import json
+import time
+from typing import List
+
+import newspaper
+
+
+def validate_article_date(art: newspaper.Article, valid_date: str):
+    if art.publish_date is not None:
+        raise ValueError("Article publish_date is None")
+    if not str(art.publish_date).startswith(valid_date):
+        raise ValueError(f"Article publish_date {art.publish_date} does not match valid_date {valid_date}")
+    
+
+def validate_article_text(art: newspaper.Article):
+    if art.text is None or len(art.text) < 100:
+        raise ValueError("Article text is None")
+
+
+def parse(parser: str, ticker: str, urls: List[str], valid_date=''):
+    for url in urls:
+        print('='*80)
+        print(f"Processing URL: {url}")
+        
+        try: 
+            article = newspaper.article(url)  
+            validate_article_date(article, valid_date)      
+            validate_article_text(article)
+        except ValueError as e:
+            print(f"Invalid article: {e} ({url})")
+            continue
+        except Exception as e:
+            print(f"Failed to parse URL: {e} ({url})")
+            continue
+        
+        # abbreviate title, max 20 characters and file system friendly
+        title = article.title[:20].replace(' ', '_')
+        # format date like '2023-02-22'
+        date = f"{article.publish_date}"
+        date = date.split(' ')[0]
+        data = {'symbol': ticker, 'text': article.text, 'date': f"{date}", 'url': url, 'title': article.title}
+        outfile = f"{ticker}_{date}_{parser}_{title}.json"
+        
+        with open(outfile, 'w') as f:            
+            json.dump(data, f, indent=4)
+            
+        print(f"Wrote file '{outfile}'")
+        
+        time.sleep(3)
+        
